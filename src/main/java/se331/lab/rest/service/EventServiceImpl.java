7b1ee8ca89cd093770a9fde596c8aa175501c110
tuffffffffffffffffy! <153575241+IngM5058@users.noreply.github.com>
2024-09-18 15:06:30 +0700

    4.6

--- src/main/java/se331/lab/rest/service/EventServiceImpl.java
+++ src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -1,35 +1,33 @@
-package se331.lab.rest.service;
+package se331.lab.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-
 import se331.lab.dao.EventDao;
-import se331.lab.rest.entity.Event;
+import se331.lab.entity.Event;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
-
-
     final EventDao eventDao;
-
-    public EventServiceImpl(EventDao eventDao) {
-        this.eventDao = eventDao;
-    }
-
     @Override
-    public Integer getEventSize() {
+    public Integer getEventSize(){
         return eventDao.getEventSize();
     }
-
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
         return eventDao.getEvents(pageSize, page);
     }
 
     @Override
-    public Event getEvent(Long id) {
-        return eventDao.getEvent(id);
+    public Event getEventById(Long id) {
+        return eventDao.getEventById(id);
+    }
+
+    @Override
+    public Event save(Event event) {
+        return eventDao.saveEvent(event);
     }
 }
